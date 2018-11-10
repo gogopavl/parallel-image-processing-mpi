00083f1a3c27a7c82942bf8a496a0c18ab6f438d
pgogousis <pgogousis@gmail.com>
2018-11-10 19:43:54 +0000

    Working version of serial coursework implementation

--- Assignment/CS_implementation/main.c
+++ Assignment/CS_implementation/main.c
@@ -4,21 +4,66 @@
 #include <math.h>
 #include "pgmio.h"
 
-#define WIDTH 192
-#define HEIGHT 128
+#define WIDTH 256
+#define HEIGHT 192
+#define MAX_ITERS 10000
 
 int main(int argc, char const *argv[])
 {
+  printf("bkpt\n");
   double image[WIDTH][HEIGHT]; // Array containing the original pgm file
+  double edge[WIDTH + 2][HEIGHT + 2];
+  double old[WIDTH + 2][HEIGHT + 2];
+  double new[WIDTH + 2][HEIGHT + 2];
 
   // Declaring input file
-  char *filename;  
-  filename = "edge192x128.pgm";
+  char *filename;
+  filename = "edge256x192.pgm";
 
   // Import file to image array
   printf("\nReading <%s>\n", filename);
   pgmread(filename, image, WIDTH, HEIGHT);
   printf("\n");
+
+  int i, j = 0;
+
+  // Copying image to edge array
+  for(i = 1 ; i < WIDTH + 1; ++i){
+    for(j = 1 ; j < HEIGHT + 1; ++j) {
+      edge[i][j] = image[i-1][j-1];
+    }
+  }
+  
+  // Initializing old with white pixels
+  for(i = 0 ; i < WIDTH + 2; ++i){
+    for(j = 0; j < HEIGHT + 2; ++j){
+      old[i][j] = 255.0;
+    }
+  }
+
+  int iter = 0;
+  // Calculating new array
+  for(iter = 0; iter < MAX_ITERS; ++iter){
+    
+    for(i = 1; i < WIDTH + 1; ++i){
+      for(j = 1; j < HEIGHT + 1; ++j){
+        new[i][j] = 0.25*(old[i-1][j] + old[i+1][j] + old[i][j-1] + old[i][j+1] - edge[i][j]);
+      }
+    }
+    for(i = 1 ; i < WIDTH + 1; ++i){
+      for(j = 1; j < HEIGHT + 1; ++j){
+        old[i][j] = new[i][j];
+      }
+    }
+  }
+
+  // Copying old/new to image - minus the halo cells
+  for(i = 1 ; i < WIDTH + 1; ++i){
+    for(j = 1; j < HEIGHT + 1; ++j){
+      image[i-1][j-1] = old[i][j];
+    }
+  }
+
   
   // Export image to output file
   char *outputfile;
