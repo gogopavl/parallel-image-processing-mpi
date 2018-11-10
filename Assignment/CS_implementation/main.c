06efd67d11550c47237c0a8246c63c328b95b397
pgogousis <pgogousis@gmail.com>
2018-11-10 12:18:18 +0000

    Restructuring directories

--- Assignment/CS_implementation/main.c
+++ Assignment/CS_implementation/main.c
@@ -2,26 +2,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-
 #include "pgmio.h"
 
-#define M 192
-#define N 128
+#define WIDTH 192
+#define HEIGHT 128
 
 int main(int argc, char const *argv[])
 {
-  double image[M][N];
+  double image[WIDTH][HEIGHT]; // Array containing the original pgm file
 
+  // Declaring input file
   char *filename;  
   filename = "edge192x128.pgm";
-  printf("\nReading <%s>\n", filename);
-  pgmread(filename, image, M, N);
-  printf("\n");
 
+  // Import file to image array
+  printf("\nReading <%s>\n", filename);
+  pgmread(filename, image, WIDTH, HEIGHT);
+  printf("\n");
+  
+  // Export image to output file
   char *outputfile;
   outputfile = "out.pgm";
   printf("\nWriting %s\n", outputfile);
-  pgmwrite(outputfile, image, M, N);
+  pgmwrite(outputfile, image, WIDTH, HEIGHT);
   
   return 0;
 }
