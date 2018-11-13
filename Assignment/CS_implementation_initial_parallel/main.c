8f0f505f6aaad3157e47c1dfff39355bd0df2a16
pgogousis <pgogousis@gmail.com>
2018-11-13 18:18:02 +0000

    Initial parallel approach implemented // Check performace and compare between serial and parallel code

--- Assignment/CS_implementation_initial_parallel/main.c
+++ Assignment/CS_implementation_initial_parallel/main.c
@@ -11,15 +11,15 @@
 #define PWIDTH WIDTH/PROCS
 #define PHEIGHT HEIGHT
 
-#define MAX_ITERS 1000
+#define MAX_ITERS 100000
 
 
 void checkNumberOfArgs(char *argument, int world_size);
 
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {  
   double master_image[WIDTH][HEIGHT]; // Array for master process
-  double image[PWIDTH][PHEIGHT]; // Array for each other process
+  double image[WIDTH][HEIGHT]; // Array for each other process
   double edge[PWIDTH + 2][PHEIGHT + 2];
   double old[PWIDTH + 2][PHEIGHT + 2];
   double new[PWIDTH + 2][PHEIGHT + 2];
@@ -32,6 +32,17 @@ int main(int argc, char const *argv[])
 
   int this_rank; // get rank id
   MPI_Comm_rank(MPI_COMM_WORLD, &this_rank);
+
+  int next_rank, previous_rank;
+
+  next_rank = this_rank + 1;
+  previous_rank = this_rank - 1;
+  if(next_rank >= world_size){
+    next_rank = MPI_PROC_NULL; // Far right and far left processes do not need to communicate sideways
+  }
+  if(previous_rank < 0){
+    previous_rank = MPI_PROC_NULL;
+  }
   
   if(this_rank == 0){
     // Declaring input file
@@ -43,29 +54,20 @@ int main(int argc, char const *argv[])
     pgmread(filename, master_image, WIDTH, HEIGHT);
     printf("\n");
   }
+
   MPI_Scatter(&master_image[0][0], PWIDTH*PHEIGHT, MPI_DOUBLE, &image[0][0], PWIDTH*PHEIGHT, MPI_DOUBLE, 0, MPI_COMM_WORLD);
   
-  /*
-  
-  
-  scattered the data, pick up from here!!!
-  
-  
-  */
-  
-  
-
   int i, j = 0;
 
   // Copying image to edge array
-  for(i = 1 ; i < WIDTH + 1; ++i){
+  for(i = 1 ; i < PWIDTH + 1; ++i){
     for(j = 1 ; j < PHEIGHT + 1; ++j) {
       edge[i][j] = image[i-1][j-1];
     }
   }
   
   // Initializing old with white pixels
-  for(i = 0 ; i < WIDTH + 2; ++i){
+  for(i = 0 ; i < PWIDTH + 2; ++i){
     for(j = 0; j < PHEIGHT + 2; ++j){
       old[i][j] = 255.0;
     }
@@ -74,33 +76,41 @@ int main(int argc, char const *argv[])
   int iter = 0;
   // Calculating new array
   for(iter = 0; iter < MAX_ITERS; ++iter){
-    
-    for(i = 1; i < WIDTH + 1; ++i){
-      for(j = 1; j < HEIGHT + 1; ++j){
+
+    MPI_Sendrecv(&old[PWIDTH][1], PHEIGHT, MPI_DOUBLE, next_rank, 1, &old[0][1], PHEIGHT, MPI_DOUBLE, previous_rank, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    MPI_Sendrecv(&old[1][1], PHEIGHT, MPI_DOUBLE, previous_rank, 2, &old[PWIDTH +1 ][1], PHEIGHT, MPI_DOUBLE, next_rank, 2, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+    for(i = 1; i < PWIDTH + 1; ++i){
+      for(j = 1; j < PHEIGHT + 1; ++j){
         new[i][j] = 0.25*(old[i-1][j] + old[i+1][j] + old[i][j-1] + old[i][j+1] - edge[i][j]);
       }
     }
-    for(i = 1 ; i < WIDTH + 1; ++i){
-      for(j = 1; j < HEIGHT + 1; ++j){
+    for(i = 1 ; i < PWIDTH + 1; ++i){
+      for(j = 1; j < PHEIGHT + 1; ++j){
         old[i][j] = new[i][j];
       }
     }
   }
 
   // Copying old/new to image - minus the halo cells
-  for(i = 1 ; i < WIDTH + 1; ++i){
-    for(j = 1; j < HEIGHT + 1; ++j){
+  for(i = 1 ; i < PWIDTH + 1; ++i){
+    for(j = 1; j < PHEIGHT + 1; ++j){
       image[i-1][j-1] = old[i][j];
     }
   }
+
+  // MPI GATHER
+  MPI_Gather(&image[0][0], PWIDTH*PHEIGHT, MPI_DOUBLE, &master_image[0][0], PWIDTH*PHEIGHT, MPI_DOUBLE, 0, MPI_COMM_WORLD);
   
-  // Export image to output file
-  char *outputfile;
-  outputfile = "out.pgm";
-  printf("\nWriting %s\n", outputfile);
-  pgmwrite(outputfile, image, WIDTH, HEIGHT);
-  
-  return 0;
+  if(this_rank == 0){
+    // Export image to output file
+    char *outputfile;
+    outputfile = "out.pgm";
+    printf("\nWriting %s\n", outputfile);
+    pgmwrite(outputfile, &master_image[0][0], WIDTH, HEIGHT);
+  }
+
+  MPI_Finalize();
 }
 
 void checkNumberOfArgs(char *argument, int world_size){
